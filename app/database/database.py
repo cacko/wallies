45ca5795fb3ccfb7dc5be322cb6cda3e6b87ab6c
cacko <alex@cacko.net>
2024-06-02 07:47:56 +0100

    TODO: write meaningful commit message

--- app/database/database.py
+++ app/database/database.py
@@ -1,8 +1,18 @@
 from playhouse.db_url import parse
 from playhouse.postgres_ext import PostgresqlExtDatabase
 from app.config import app_config
-from typing import Optional
+from typing import Optional, Any
+from psycopg2 import OperationalError
 
+
+class ReconnectingDB(PostgresqlExtDatabase):
+    
+    def execute_sql(self, sql, params: Any | None = ..., commit=...):
+        try:
+            return super().execute_sql(sql, params, commit)
+        except OperationalError as e:
+            print(e)
+            raise RuntimeError
 
 class DatabaseMeta(type):
     _instance: Optional['Database'] = None
@@ -13,7 +23,7 @@ class DatabaseMeta(type):
         return cls._instance
 
     @property
-    def db(cls) -> PostgresqlExtDatabase:
+    def db(cls) -> ReconnectingDB:
         return cls().get_db()
 
 
@@ -21,7 +31,7 @@ class Database(object, metaclass=DatabaseMeta):
 
     def __init__(self):
         parsed = parse(app_config.db.url)
-        self.__db = PostgresqlExtDatabase(**parsed)
+        self.__db = ReconnectingDB(**parsed)
 
-    def get_db(self) -> PostgresqlExtDatabase:
+    def get_db(self) -> ReconnectingDB:
         return self.__db
